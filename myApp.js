69fa88c8e7a28b1c46a4c46cfac4b727e486a450
John Acera <john.acera@sportality.se>
2023-02-26 09:47:52 +0800

    added static css file

--- myApp.js
+++ myApp.js
@@ -2,9 +2,10 @@ let express = require('express');
 let app = express();
 
 console.log('Hello World');
+app.use('/', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-  const absolutePath = __dirname + '/views/index.html'
+  const absolutePath = __dirname + '/views/index.html';
   res.sendFile(absolutePath);
 });
 
